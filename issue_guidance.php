374cbeb2ee8b9a991fdfbc1628161f2ce9b3ec95
stewartweiss <sweiss@hunter.cuny.edu>
2019-11-17 13:51:02 -0500

    Updated the HFOSS slides and issue guidance

---
+++ issue_guidance.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+ 
+<head>
+<meta name="revisit-after" content="14 days">
+<meta name="keywords" content="Stewart Weiss, Hunter College, computer science, open source, open source software">
+<meta name="description" content="Stewart Weiss, PHD Courant Institute, Professor Hunter College"> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="../styles/styles.css" rel="stylesheet" type="text/css">   
+</head>
+
+
+<body>
+<?php require("../menu.html"); ?>
+
+<div id="main_body" style="width:100%">
+
+<h1 id="guidance-for-writing-issues">Guidance for Writing Issues</h1>
+<p>The purpose of this very brief document is to give you some pointers for writing up issues to submit to a project.</p>
+<ol style="list-style-type: decimal">
+<li>Make sure that you read the CONTRIBUTING document for the project, if it exists, to see if the project expects issues to be reported in a specific format or to contain specific information.</li>
+<li>Make sure the issue has not been reported already by searching through the issue tracker for terms related to what you are about to report.</li>
+<li>Make sure to identify in the title of the issue whether it is
+<ul>
+<li>a defect (bug) in code:<br> This can be manifested by program malfunctioning if source code, or if a document, incorrect information, spelling errors, etc.</li>
+<li>a design flaw:<br> This can be a program that does not malfunction but whose behavior is not the natural or expected behavior. In a document, it is an editorial change, like suggesting different language for a paragraph.</li>
+<li>feature request:<br> There is no malfunctioning, but the program does not do something that you think would be a nice addition. With a document, it is a suggestion to add some ideas or text that improves the flow or supports soe premise, and so on.</li>
+</ul></li>
+<li>Make sure that</p>
+<ul>
+<li>it is reproducible:<br> If the developers cannot reproduce it or conclusively prove that it exists, they probably will not be able to fix it, and move on to the next bug. Provide step-by-step instructions for reproducing the bug for a quicker resolution. Attach or link to videos and screenshots if that would be helpful. For documents, this is not relevant.</li>
+<li>it is specific:<br> Try and figure out exactly what causes the problem. Do not report more than one issue in the same report. You should report each issue separately. For documents, be specific about where in the document the issue is.</li>
+<li>you describe your environment:<br> Include information about the version of the software or document, operating system, and other components on which execution might rely.</li>
+<li>you write a good title:<br> You should be as precise and clear as possible in the title. Titles such as &quot;Program hangs&quot; or &quot;It doesn't work&quot; are not useful.</li>
+<li>if it is a bug and the issue tracker has bug description fields:<br> Make sure you complete the fields.</li>
+</ul></li>
+</ol>
+</div>
+
+<!-- include navigation bar, footer, and statcounter -->
+<?php include_once('../pagebottom.php'); ?> 
+
+
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" >
+</a>
+
+<a href="http://validator.w3.org/check?uri=referer">
+    <img src="http://www.w3.org/Icons/valid-html401-blue"
+        alt="Valid HTML 4.01 Transitional" style="border:0;width:88px;height:31px">
+</a>
+
+</body>
+</html>
